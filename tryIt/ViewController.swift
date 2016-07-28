feefe53d346aa90cf9989d21df65b2aa8bb0f064
Jeshal Mehta <jeshalmehta@jeshals-macbook-pro.local>
2016-07-28 11:36:54 +0530

    Initial Commit

---
+++ tryIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tryIt
+//
+//  Created by Jeshal Mehta on 28/07/16.
+//  Copyright © 2016 Jeshal Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
